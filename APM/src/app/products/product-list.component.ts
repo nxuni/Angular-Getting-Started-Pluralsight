5ec1fadf6d5ab92bba584514470a0b2d1643f289
Carlos Alberto Benitez Martin <ca.benitez.m@gmail.com>
2021-05-21 20:55:38 -0500

    Retrieving Data Using HTTP

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -7,12 +8,14 @@ import { ProductService } from "./product.service";
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductlistComponent implements OnInit {
+export class ProductlistComponent implements OnInit, OnDestroy {
         
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
     
     private _listFilter: string = '';
     public get listFilter(): string {
@@ -32,23 +35,33 @@ export class ProductlistComponent implements OnInit {
      */
     constructor(private productService: ProductService) {
         
+    }    
+    ngOnInit(): void {
+        this.sub = this.productService.getProducts()
+            .subscribe({
+                next: products => {
+                    this.products = products;
+                    this.filteredproducts = this.products;
+
+                }
+                , error: err => this.errorMessage = err
+            });
     }
 
-    ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredproducts = this.products;        
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
-    
+
     toggleImage(): void {
         this.showImage = !this.showImage;
-    }
-    
+    }    
+
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
             product.productName.toLocaleLowerCase().includes(filterBy));
-    }
-    
+    }    
+
     onRatingClicked(message: string): void {
         this.pageTitle = 'Product list: ' + message;
     }
--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,35 +1,39 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
 import { IProduct } from "./product";
+import { catchError, tap } from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
+    private productURL = 'api/products/products.json';
+    /**
+     *
+     */
+    constructor(private http: HttpClient) { }
     /**
      * Get products
      */
-    public getProducts(): IProduct[] {
-        return [
-            {
-                "productId": 1,
-                "productName": "Leaf Rake",
-                "productCode": "GDN-0011",
-                "releaseDate": "March 19, 2019",
-                "description": "Leaf rake with 48-inch wooden handle.",
-                "price": 19.95,
-                "starRating": 3.2,
-                "imageUrl": "assets/images/leaf_rake.png"
-            },
-            {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2019",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": 32.99,
-                "starRating": 4.2,
-                "imageUrl": "assets/images/garden_cart.png"
-            }
-        ];
+    public getProducts(): Observable<IProduct[]> {
+        return this.http
+        .get<IProduct[]>(this.productURL)
+        .pipe(
+            tap(data => console.log('All', JSON.stringify(data)))
+            , catchError(this.handleError)
+        );
     }
+    handleError(err: HttpErrorResponse) {
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+            errorMessage = `An error occurred: ${err.error.message}`;
+        }
+        else {
+            errorMessage = `Server returned code: ${err.status}, Error massage is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+    }
+    
 }
