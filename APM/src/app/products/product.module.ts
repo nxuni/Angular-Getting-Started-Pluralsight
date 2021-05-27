82b023631db21ab324edcad24db430a4b1b67953
Carlos Alberto Benitez Martin <ca.benitez.m@gmail.com>
2021-05-27 17:12:59 -0500

    Shared Module

--- APM/src/app/products/product.module.ts
+++ APM/src/app/products/product.module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductlistComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductlistComponent
     , ProductDetailComponent
     , ConvertToSpacesPipe
-    , StarComponent
-
   ],
   imports: [
-    CommonModule
-    , FormsModule
-    , RouterModule.forChild([
+     RouterModule.forChild([
       { path: 'products', component: ProductlistComponent }
       , { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent }
-    ])
+    ]), SharedModule
 ]
 })
 export class ProductModule { }
